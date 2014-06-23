32dcce582adae1e6bfcd421a031e03e9ae17185f
2GMon <2gmon.t@gmail.com>
2014-06-23 23:50:31 +0900

    harib13e

--- bootpack.h
+++ bootpack.h
@@ -17,8 +17,9 @@ void asm_inthandler2c(void);
 struct FIFO32 {
     int *buf;
     int p, q, size, free, flags;
+    struct TASK *task;
 };
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf);
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task);
 int fifo32_put(struct FIFO32 *fifo, int data);
 int fifo32_get(struct FIFO32 *fifo);
 int fifo32_status(struct FIFO32 *fifo);
@@ -183,8 +184,10 @@ void timer_settime(struct TIMER *timer, unsigned int timeout);
 void inthandler20(int *esp);
 
 /* mtask.c */
-#define MAX_TASKS   1000 /* 最大タスク数 */
-#define TASK_GDT0   3 /* TSSをGDTの何番から割り当てるのか */
+#define MAX_TASKS       1000 /* 最大タスク数 */
+#define TASK_GDT0       3 /* TSSをGDTの何番から割り当てるのか */
+#define MAX_TASKS_LV    100
+#define MAX_TASKLEVELS  10
 struct TSS32 {
     int backlink, esp0, ss0, esp1, ss1, esp2, ss2, cr3;
     int eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
@@ -193,16 +196,23 @@ struct TSS32 {
 };
 struct TASK {
     int sel, flags; /* selはGDTの番号のこと */
+    int level, priority;
     struct TSS32 tss;
 };
-struct TASKCTL {
+struct TASKLEVEL {
     int running; /* 動作しているタスクの数 */
     int now; /* 現在動作しているタスクがどれだか分かるようにするための変数 */
-    struct TASK *tasks[MAX_TASKS];
+    struct TASK *tasks[MAX_TASKS_LV];
+};
+struct TASKCTL {
+    int now_lv; /* 現在動作中のレベル */
+    char lv_change; /* 次回タスクスイッチのときに、レベルも変えたほうがいいかどうか */
+    struct TASKLEVEL level[MAX_TASKLEVELS];
     struct TASK tasks0[MAX_TASKS];
 };
 extern struct TIMER *task_timer;
 struct TASK *task_init(struct MEMMAN *memman);
 struct TASK *task_alloc(void);
-void task_run(struct TASK *task);
+void task_run(struct TASK *task, int level, int priority);
 void task_switch(void);
+void task_sleep(struct TASK *task);
--- fifo.c
+++ fifo.c
@@ -7,7 +7,7 @@
 #define FLAGS_OVERRUN 0x0001
 
 /* FIFOバッファの初期化 */
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf)
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task)
 {
     fifo->size = size;
     fifo->buf = buf;
@@ -15,6 +15,7 @@ void fifo32_init(struct FIFO32 *fifo, int size, int *buf)
     fifo->flags = 0;
     fifo->p = 0; /* 書き込み位置 */
     fifo->q = 0; /* 読み込み位置 */
+    fifo->task = task; /* データが入ったときに起こすタスク */
     return;
 }
 
@@ -32,6 +33,11 @@ int fifo32_put(struct FIFO32 *fifo, int data)
         fifo->p = 0;
     }
     fifo->free--;
+    if (fifo->task != 0) {
+        if (fifo->task->flags != 2) { /* タスクが寝ていたら */
+            task_run(fifo->task, -1, 0); /* 起こしてあげる */
+        }
+    }
     return 0;
 }
 
