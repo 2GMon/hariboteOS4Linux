2edde6f12cada6cb018441c71d7c1f16eae9089a
2GMon <2gmon.t@gmail.com>
2014-06-29 01:24:50 +0900

    fixed message

--- console.c
+++ console.c
@@ -330,7 +330,7 @@ int *inthandler0d(int *esp)
 {
     struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
     struct TASK *task = task_now();
-    cons_putstr0(cons, "¥nINT 0D :¥n General Protected Exception.¥n");
+    cons_putstr0(cons, "\nINT 0D :\n General Protected Exception.\n");
     return &(task->tss.esp0);	/* 異常終了させる */
 }
 
