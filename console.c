78058fd9dd912c511cb4cd7221649f95860f5b82
2GMon <2gmon.t@gmail.com>
2014-07-06 00:38:52 +0900

    show EIP Address When Catch a Exception

--- console.c
+++ console.c
@@ -358,7 +358,10 @@ int *inthandler0d(int *esp)
 {
     struct CONSOLE *cons = (struct CONSOLE *) *((int *) 0x0fec);
     struct TASK *task = task_now();
+    char s[30];
     cons_putstr0(cons, "\nINT 0D :\n General Protected Exception.\n");
-    return &(task->tss.esp0);	/* 異常終了させる */
+    sprintf(s, "EIP = %08X¥n", esp[11]);
+    cons_putstr0(cons, s);
+    return &(task->tss.esp0);   /* 異常終了させる */
 }
 
