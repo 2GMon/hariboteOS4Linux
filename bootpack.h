5279fb8aace7053d95f5ade9c34098870279e1af
2GMon <2gmon.t@gmail.com>
2014-08-12 23:27:07 +0900

    harib23i

--- bootpack.h
+++ bootpack.h
@@ -255,6 +255,8 @@ void cmd_mem(struct CONSOLE *cons, unsigned int memtotal);
 void cmd_cls(struct CONSOLE *cons);
 void cmd_dir(struct CONSOLE *cons);
 void cmd_type(struct CONSOLE *cons, int *fat, char *cmdline);
+void cmd_exit(struct CONSOLE *cons, int *fat);
+void cmd_start(struct CONSOLE *cons, char *cmdline, int memtotal);
 void cmd_hlt(struct CONSOLE *cons, int *fat);
 int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int eax);
 int *inthandler0d(int *esp);
